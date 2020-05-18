436de4950f38afc4f47a0ae1fcbf80f93d5cc712
createandbuild <33373263+createandbuild@users.noreply.github.com>
2020-05-18 00:21:24 -0400

    styling for sota.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-sota.barChart({ selector: "#module-general-class-d3", dataFile: "data/general/class", totalResp: 1052, maxVal: true, displayPercentage: true });
+let colorsArray = {
+    "general": ["#234F84","#0f64a0","#0087bf","#54a5cf","#86c6e3","#b4dbea","#e4f2f7"],
+    "politics": ["#660066", "#7a2475", "#935d96", "#a880ac", "#bd98ba", "#dac7d8"],
+    "wellness": ["#6cb643", "#99c872", "#b2d593", "#cae3cb"],
+    "sex": ["#c6307c", "#d5739d", "#e1aac0", "#f0d3dc"],
+    "drugs": ["#e77929", "#efa05e", "#f3b572", "#f6cc99", "#fbe3c4"],
+    "discipline": ["#b43432", "#d47173", "#e4a8a6", "#f0d1ca"]
+}
+
+sota.setColors(colorsArray);
+
+sota.setStyles(); // for custom sotaConfig; for default options, don't pass any params
+
+sota.barChart({ selector: "#module-general-class-d3", dataFile: "data/general/class", totalResp: 1052, displayPercentage: true });
 
 sota.barChart({ selector: "#module-general-ethnicity-d3", dataFile: "data/general/ethnicity", totalResp: 1052, maxVal: true, displayPercentage: true });
 
