d7215da2085df020b23175807b9be1fda173a088
createandbuild <33373263+createandbuild@users.noreply.github.com>
2020-05-17 23:07:57 -0400

    first test

---
+++ index.js
@@ -0,0 +1,20 @@
+sota.barChart({ selector: "#module-general-class-d3", dataFile: "data/general/class", totalResp: 1052, maxVal: true, displayPercentage: true });
+
+sota.barChart({ selector: "#module-general-ethnicity-d3", dataFile: "data/general/ethnicity", totalResp: 1052, maxVal: true, displayPercentage: true });
+
+sota.stackedBarChart({ selector: "#module-general-legacy-income-d3", dataFile: "data/general/legacyXincome", labelStyle: "onBar", showLegend: true })
+// sota.pieChart({ selector: "#module-general-community-d3", dataFile: "data/community"});
+//
+// sota.lineGraph({ selector: "#module-discipline-time-d3", dataFile: "data/disc-time", inputIsPercentage: true, maxVal: 8 })
+//
+// sota.stackedBarChart({ selector: "#module-discipline-room-visits", dataFile: "data/room-visit-policy", groupLabelStyle: "onBar" })
+//
+// sota.stackedBarChart({ selector: "#module-general-parents-college", dataFile: "data/parents-college", labelStyle: "aboveBar", showLegend: false })
+//
+// sota.customBarChart({ selector: "#wellness-cloud-svg", dataFile: "data/happiness", shapeFile: "cloud" })
+//
+// sota.columnChart({ selector: "#wellness-support", dataFile: "data/support", totalResp: 1052 })
+//
+// sota.groupedBarChart({ selector: "#wellness-social-media", dataFile: "data/wellness-social-media", totalResp: {2022:214,2021:275,2020:271,2019:286} })
+//
+// sota.stackedColumnChart({ selector: "#module-politics-reverse-racism-gender", dataFile: "data/reverse-racism-gender", totalResp: 1032 })
