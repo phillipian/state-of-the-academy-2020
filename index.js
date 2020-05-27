6ef6013798281862bcd903c59b84ef20e13a7e02
createandbuild <33373263+createandbuild@users.noreply.github.com>
2020-05-27 00:19:31 -0400

    updated and added colors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let colorsArray = {
     "general": ["#235085","#1065a1","#0b88c0","#22a7d3","#4ac2e5","#8dd1e9","#c6e6f3","#e3f4fb"],
-    "politics": ["#660066", "#7a2475", "#935d96", "#a880ac", "#bd98ba", "#dac7d8"],
-    "wellness": ["#6cb643", "#99c872", "#b2d593", "#cae3cb"],
-    "sex": ["#c6307c", "#d5739d", "#e1aac0", "#f0d3dc"],
-    "drugs": ["#e77929", "#efa05e", "#f3b572", "#f6cc99", "#fbe3c4"],
-    "discipline": ["#b43432", "#d47173", "#e4a8a6", "#f0d1ca"]
+    "politics": ["#2C3278", "#5D5797", "#7D78AD", "#948EB9", "#A8A3C6", "#DFDBE7"],
+    "campus": ["#762670", "#945E98", "#A77FAD", "#C0ABC7"],
+    "school": ["#812F54", "#AA7281", "#BF97A0", "#DCC5C3"],
+    "discipline": ["#B53432", "#D47173", "#E4A8A6", "#F0D1CA"],
+    "sex": ["#C7327E", "#D6739D", "#E1AAC0", "#F0D4DD"],
+    "drugs": ["#E77929", "#EFA05E", "#F3B572", "#F6CC99", "#FAE3C4"],
+    "wellness": ["#6CB645", "#99C872", "#B2D593", "#CAE2CB"],
+    "covid": ["#008244", "#39B074", "#7CC193", "#C8E6DD"],
+    "community": ["#089181", "#36B39F", "#95CFC3", "#C8E6DD"]
 }
 
 sota.setColors(colorsArray);
@@ -58,8 +62,6 @@ sota.stackedBarChart({ selector: "#module-general-legacy-income-d3", dataFile: "
 "module-general-varisty-income-d3"  //stacked bar
 
 "module-general-incarcerated-d3"  //pie
-
-
 
 // sota.pieChart({ selector: "#module-general-community-d3", dataFile: "data/community"});
 //
