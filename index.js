950f0e72b3a5a106d5e2e67d5b0246ca9b37e338
createandbuild <33373263+createandbuild@users.noreply.github.com>
2020-05-31 05:03:09 -0400

    finished politics

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let colorsArray = {
     "general": ["#235085","#1065a1","#0b88c0","#22a7d3","#4ac2e5","#8dd1e9","#c6e6f3","#e3f4fb"],
-    "politics": ["#2C3278", "#5D5797", "#7D78AD", "#948EB9", "#A8A3C6", "#DFDBE7"],
-    "campus": ["#762670", "#945E98", "#A77FAD", "#C0ABC7"],
-    "school": ["#812F54", "#AA7281", "#BF97A0", "#DCC5C3"],
-    "discipline": ["#B53432", "#D47173", "#E4A8A6", "#F0D1CA"],
-    "sex": ["#C7327E", "#D6739D", "#E1AAC0", "#F0D4DD"],
+    "politics": ["#2C3278", "#5D5797", "#7D78AD", "#948EB9", "#A8A3C6", "#BFBDD5", "#DFDBE7"],
+    "campus": ["#762670", "#945E98", "#A77FAD", "#BC98B9", "#D9C6D8"],
+    "school": ["#812F54", "#AA7281", "#BF97A0", "#BF97A0", "#DCC5C3"],
+    "discipline": ["#B53432", "#C74F50", "D47173","#E4A8A6", "#F0D1CA"],
+    "sex": ["#D5127D", "#DF5694", "#E680A9", "#ECA8C1", "#F8D3DD"],
     "drugs": ["#E77929", "#EFA05E", "#F3B572", "#F6CC99", "#FAE3C4"],
-    "wellness": ["#6CB645", "#99C872", "#B2D593", "#CAE2CB"],
-    "covid": ["#008244", "#39B074", "#7CC193", "#C8E6DD"],
-    "community": ["#089181", "#36B39F", "#95CFC3", "#C8E6DD"]
+    "wellness": ["#69B645", "#98C970", "#B1D281", "#CEDFA6", "E3EFCE"],
+    "covid": ["#088446", "#32A068", "#7CC193", "#B2DAB8", "E2F1E1"],
+    "community": ["#279680", "#29AD97", "#78C4B1", "#AEDCCA", "#C8E7DD"]
 }
 
 sota.setColors(colorsArray);
@@ -40,7 +40,6 @@ sota.barChart({ selector: "#module-general-romantic-d3", dataFile: "data/general
 
 sota.columnChart({ selector: "#module-general-race-d3", dataFile: "data/general/race", totalResp: 1029 });
 
-// tick frequency too high
 sota.barChart({ selector: "#module-general-ethnicity-d3", dataFile: "data/general/ethnicity", totalResp: 1039, displayPercentage: true });
 
 sota.barChart({ selector: "#module-general-religion-d3", dataFile: "data/general/religion", totalResp: 1038, displayPercentage: true });
@@ -53,10 +52,23 @@ sota.pieChart({ selector: "#module-general-varsity-d3", dataFile: "data/general/
 
 sota.pieChart({ selector: "#module-general-income-d3", dataFile: "data/general/income"});
 
-// label goes off the page
 sota.columnChart({ selector: "#module-general-finAid-d3", dataFile: "data/general/financial_aid", totalResp: 1037 });
 
-// "module-general-socioeconomic-income-d3"  // several pies
+sota.pieChart({ selector: "#module-general-socioeconomicXincome0-d3", dataFile: "data/general/socioeconomicXincome0"});
+
+sota.pieChart({ selector: "#module-general-socioeconomicXincome1-d3", dataFile: "data/general/socioeconomicXincome1"});
+
+sota.pieChart({ selector: "#module-general-socioeconomicXincome2-d3", dataFile: "data/general/socioeconomicXincome2"});
+
+sota.pieChart({ selector: "#module-general-socioeconomicXincome3-d3", dataFile: "data/general/socioeconomicXincome3"});
+
+sota.pieChart({ selector: "#module-general-socioeconomicXincome4-d3", dataFile: "data/general/socioeconomicXincome4"});
+
+sota.pieChart({ selector: "#module-general-socioeconomicXincome5-d3", dataFile: "data/general/socioeconomicXincome5"});
+
+sota.pieChart({ selector: "#module-general-socioeconomicXincome6-d3", dataFile: "data/general/socioeconomicXincome6"});
+
+sota.pieChart({ selector: "#module-general-socioeconomicXincome7-d3", dataFile: "data/general/socioeconomicXincome7"});
 
 sota.stackedBarChart({ selector: "#module-general-legacy-income-d3", dataFile: "data/general/legacyXincome", labelStyle: "onBar", groupLabelStyle:"onBar", showLegend: true })
 
@@ -67,7 +79,7 @@ sota.pieChart({ selector: "#module-general-incarcerated-d3", dataFile: "data/gen
 // POLITICS AND WORLDVIEW
 sota.pieChart({ selector: "#module-politics-affiliation-d3", dataFile: "data/politics/political_affiliation"});
 
-sota.columnChart({ selector: "#module-politics-news_sources-d3", dataFile: "data/politics/news_sources", totalResp: 1032, displayPercentage: true });
+sota.barChart({ selector: "#module-politics-news_sources-d3", dataFile: "data/politics/news_sources", totalResp: 1032, displayPercentage: true });
 
 sota.pieChart({ selector: "#module-politics-affirmative_action-d3", dataFile: "data/politics/affirmative_action"});
 
@@ -97,28 +109,28 @@ sota.stackedBarChart({ selector: "#module-politics-politicsXleadership-d3", data
 
 sota.pieChart({ selector: "#module-politics-informed-d3", dataFile: "data/politics/informed"});
 
-// sota.stackedBarChart({ selector: "#module-politics-news_sourcesXpolitics-d3", dataFile: "data/politics/news_sourcesXpolitics", labelStyle: "onBar", groupLabelStyle:"onBar", showLegend: true });
 sota.stackedBarChart({ selector: "#module-politics-politicsXnews_sources-d3", dataFile: "data/politics/politics_old/news_sourcesXpolitics", labelStyle: "onBar", groupLabelStyle:"onBar", showLegend: true });
 
-sota.columnChart({ selector: "#module-politics-top_5_news_sources-d3", dataFile: "data/politics/top_5_news_sources", totalResp: 1037 });
+sota.columnChart({ selector: "#module-politics-top_5_news_sources-d3", dataFile: "data/politics/top_5_news_sources", totalResp: 1031 });
 
-// module-politics-news_sources-d3 // need total numbers
+sota.stackedColumnChart({ selector: "#module-politics-affirmative_actionXclass-d3", dataFile: "data/politics/affirmative_actionXclass" })
 
-sota.stackedColumnChart({ selector: "#module-politics-affirmative_actionXclass-d3", dataFile: "data/politics/affirmative_actionXclass", totalResp: 1023 })
+sota.stackedColumnChart({ selector: "#module-politics-affirmative_actionXrace-d3", dataFile: "data/politics/affirmative_actionXrace" })
 
-sota.stackedColumnChart({ selector: "#module-politics-affirmative_actionXrace-d3", dataFile: "data/politics/affirmative_actionXrace", totalResp: 1012 })
-
-sota.stackedColumnChart({ selector: "#module-politics-reverse_racismXclass-d3", dataFile: "data/politics/reverse_racismXclass", totalResp: 1026 })
+sota.stackedColumnChart({ selector: "#module-politics-reverse_racismXclass-d3", dataFile: "data/politics/reverse_racismXclass" })
 
 sota.stackedBarChart({ selector: "#module-politics-womens_rightsXfeminist-d3", dataFile: "data/politics/womens_rightsXfeminist", labelStyle: "onBar", groupLabelStyle:"onBar", showLegend: true });
 
 sota.stackedBarChart({ selector: "#module-politics-feministXgender-d3", dataFile: "data/politics/feministXgender", labelStyle: "onBar", groupLabelStyle:"onBar", showLegend: true });
 
-sota.stackedColumnChart({ selector: "#module-politics-abortionXgender-d3", dataFile: "data/politics/abortionXgender", totalResp: 1015 })
+sota.stackedColumnChart({ selector: "#module-politics-abortionXgender-d3", dataFile: "data/politics/abortionXgender" })
 
 sota.stackedBarChart({ selector: "#module-politics-prison_reformXincarceration-d3", dataFile: "data/politics/prison_reformXincarceration", labelStyle: "onBar", groupLabelStyle:"onBar", showLegend: true });
 
-sota.stackedColumnChart({ selector: "#module-politics-firearmsXcommunity-d3", dataFile: "data/politics/firearmsXcommunity", totalResp: 1032 })
+sota.stackedColumnChart({ selector: "#module-politics-firearmsXcommunity-d3", dataFile: "data/politics/firearmsXcommunity" })
+
+// CAMPUS CULTURE
+
 
 // sota.pieChart({ selector: "#module-general-community-d3", dataFile: "data/community"});
 //
